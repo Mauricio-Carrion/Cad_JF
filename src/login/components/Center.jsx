e6d617757a0bf3f14a7c100d1d0c1646f96df416
Mauricio-Carrion <mauricio.sjn@gmail.com>
2022-11-10 13:42:26 -0300

    adicionando form login

--- src/login/components/Center.jsx
+++ src/login/components/Center.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './Center.css'
 import Background from './Background'
 import Nav from './Nav'
+import Content from './Content'
 
 export default props =>
   <div className="center">
     <Background />
     <Nav />
+    <Content />
   </div>
---
+++ src/login/components/Content.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import './Content.css'
+import Logo from './Logo';
+
+export default props =>
+  <Fragment>
+    <main className="content">
+      <Logo />
+      TESTE
+    </main>
+  </Fragment>
