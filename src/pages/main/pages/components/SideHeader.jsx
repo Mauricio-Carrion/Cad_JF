2e88df51e8c2f07babb8f1b6a8ead0c87396e298
Mauricio <mauricio.sjn@gmail.com>
2022-11-14 18:34:46 -0300

    trabalhando no navbar

---
+++ src/pages/main/pages/components/SideHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SideHeader.css'
+
+const SideHeader = () => {
+  <aside className="sideheader">
+    <nav>
+      <a href="/">Usuários</a>
+      <a href="/">Clientes</a>
+      <a href="/">Visitas</a>
+      <a href="/">Relatórios</a>
+      <a href="/">Logs</a>
+      <a href="/">Sair</a>
+    </nav>
+  </aside>
+}
